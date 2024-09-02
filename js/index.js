3a10bb8672db89ece94cff39ce1b45e8924b8ee8
Iarley-01 <yarleyalves2016@gmail.com>
2024-09-02 20:31:32 -0300

    criando validação para email no newsletter

--- js/index.js
+++ js/index.js
@@ -10,3 +10,24 @@ window.onscroll = () => {
   menu.classList.remove("bx-x");
   navbar.classList.remove("active");
 }
+
+let emailInput = document.getElementById("email");
+let newsletterBox = document.querySelector(".newsletter .box");
+let errorMessage = document.querySelector(".error-message");
+
+emailInput.addEventListener("input", (e) => {
+  let inputValue = e.target.value;
+  let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  if (inputValue == "") {
+    newsletterBox.classList.add("error");
+    errorMessage.textContent = "O campo não pode ficar vazio";
+  } else if (!regex.test(inputValue)) {
+    newsletterBox.classList.add("error");
+    errorMessage.textContent = "Insira um email no formato nome@gmail.com";
+  }
+  else {
+    newsletterBox.classList.remove("error");
+    errorMessage.textContent = "";
+  }
+})
